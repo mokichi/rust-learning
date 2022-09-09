a0c8a22a796c8520afa08e33b8daa1fd97d7c686
SAITO Kazuya <saito@smartalgorithm.co.jp>
2022-09-09 15:46:29 +0900

    12. minigrep
    https://doc.rust-jp.rs/book-ja/ch12-00-an-io-project.html

---
+++ 12_minigrep/src/main.rs
@@ -0,0 +1,21 @@
+// Rust 2018 以降、クレートをインポートするための `extern crate` は不要
+// https://doc.rust-jp.rs/edition-guide/rust-2018/path-changes.html#%E3%81%95%E3%82%88%E3%81%86%E3%81%AA%E3%82%89extern-crate
+// extern crate minigrep;
+
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
